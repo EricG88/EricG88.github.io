49a8bc89e4047f1112abd1f83a718f187d7c8dac
Eric Gossett <ericgossett@Erics-MacBook-Pro.local>
2019-02-14 16:48:53 -0600

    style update

---
+++ app.js
@@ -0,0 +1,63 @@
+var fadable = {
+
+    fadeDistance: 200,
+    fadeClass   : 'fadable--invisible',
+    vpHeight    : null,
+    fadeLine    : null,
+    fadables    : null,
+    resizeTimer : null,
+  
+    initialize: function() {
+      fadable.inspectEnvironment();
+      fadable.selectFadables();
+      fadable.prepareFadables();
+      fadable.bindEvents();
+    },
+  
+    inspectEnvironment: function() {
+      fadable.vpHeight = window.innerHeight;
+      fadable.fadeLine = fadable.vpHeight - fadable.fadeDistance;
+    },
+  
+    selectFadables: function() {
+      fadable.fadables = document.querySelectorAll('.fadable');
+    },
+  
+    prepareFadables: function() {
+      Array.prototype.forEach.call(fadable.fadables, function(el, i) {
+        if (el.getBoundingClientRect().top > fadable.vpHeight) {
+          el.classList.add(fadable.fadeClass);
+        }
+      });
+    },
+  
+    showInvisibles: function() {
+      Array.prototype.forEach.call(fadable.fadables, function(el, i) {
+        if (el.getBoundingClientRect().top < fadable.fadeLine) {
+          el.classList.remove(fadable.fadeClass);
+        }
+      });
+    },
+  
+    bindEvents: function() {
+      document.addEventListener('scroll', function() {
+        fadable.showInvisibles();
+      });
+  
+      window.addEventListener('resize', function() {
+        clearTimeout(fadable.resizeTimer);
+        fadable.resizeTimer = setTimeout(function() {
+          fadable.inspectEnvironment();
+          fadable.prepareFadables();
+          fadable.showInvisibles();
+        }, 250);
+      });
+    }
+  
+  }
+  
+  document.onreadystatechange = function() {
+    if (document.readyState == 'complete') {
+      fadable.initialize();
+    }
+  }
